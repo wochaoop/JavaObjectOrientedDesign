820683e874cae60532f6dc2b5af4c8245cf3d21c
xiwangly2 <xiwangly2@gmail.com>
2024-08-06 09:16:39 +0800

    提交：测试

---
+++ src/main/java/com/web/Test11111.java
@@ -0,0 +1,8 @@
+package com.web;
+
+public class Test11111 {
+    public static void main(String[] args) {
+        String homeDir = System.getProperty("user.home");
+        System.out.println(homeDir);
+    }
+}
